e8f38ff499c160a2ce1db383df0bbdc73d204335
devXamr <devxamr@gmail.com>
2025-06-05 16:45:39 +0530

    ui refined

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,45 @@
 import {useState} from "react";
 //@ts-ignore
 import axios, * as other from "axios"
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import Image from 'next/image'
+import logo from '../../public/logowithtext.png'
+
 
 
 export default function Home() {
     const [reelUrl, setReelUrl] = useState('')
     const [downloadUrl, setDownloadUrl] = useState('')
+    const [submitted, setSubmitted] = useState(false)
+    const [error, setError] = useState(false)
+
+    async function downloadReel() {
+        try {
+            const response = await fetch(downloadUrl);
+            const blob = await response.blob();
+
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = 'instagram-reel.mp4';
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+            URL.revokeObjectURL(url);
+        } catch (err) {
+            console.error("Download failed", err);
+        }
+    }
+
 
     async function handleSubmit(){
+        if(reelUrl === ''){
+            setError(true)
+            return
+        }
+
+        setError(false)
+
         const options = {
             method: 'GET',
             url: 'https://instagram-reels-downloader-api.p.rapidapi.com/download',
@@ -32,29 +64,48 @@ export default function Home() {
         } catch (error) {
             console.error(error);
         }
-
+       setSubmitted(true)
+       setReelUrl('')
     }
 
     return (
-    <div>
-        <div>Insta Reel Downloader</div>
-        <div>Paste in your instagram reel below</div>
+        <div className='font-primary py-20 max-w-[90%] lg:max-w-[25%] mx-auto'>
+            <Image className='w-1/2 mx-auto mb-3' src={logo} alt='reel quick logo' />
+            <div className='text-center text-red-500 text-3xl font-semibold'>Insta Reel Downloader</div>
+            <div className='text-gray-600 text-sm text-center mt-2  mx-auto'>Download any instagram reel at command.
+                Paste a link and it will be ready for download, ReelQuick.
+            </div>
 
-        <form onSubmit={(e) => {
-            e.preventDefault()
-            handleSubmit()
-        }}>
-            <input value={reelUrl} onChange={(e) => setReelUrl(e.target.value)} type='text' className='block' placeholder='place any instagram reel url here'/>
-            <button type='submit' className='block'>Submit</button>
-        </form>
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                handleSubmit()
+            }}>
+                <input value={reelUrl} onChange={(e) => setReelUrl(e.target.value)} type='text'
+                       className='block w-full mx-auto mt-5 pr-5 pl-1 py-3 border border-gray-200 text-sm rounded-lg'
+                       placeholder='paste url here'/>
+                {error && <div className='text-red-500 text-xs'>The field cannot be empty</div>}
+                <button type='submit'
+                        className='transition-colors duration-100 ease-in-out text-center w-full py-3 bg-red-600  text-white rounded-lg hover:bg-gradient-to-b cursor-pointer mt-4 hover:from-red-400 via-red-500 to-red-600 text-sm font-semibold'>Submit
+                </button>
+            </form>
+
+            {submitted && downloadUrl && <div className='flex mt-10 pl-4  rounded-xl bg-red-100'>
+                <div>
+                    <div className='mt-5 text-sm mb-4 text-gray-700'>Your video is ready for download!</div>
+                    <button onClick={downloadReel}
+                            className='px-5 py-2 cursor-pointer text-sm rounded-lg bg-black text-white'>Download Reel
+                    </button>
+                </div>
+                <video controls width='100%' src={downloadUrl} className='w-[50%] mx-auto rounded-tr-xl'/>
+
+            </div>}
+
+            {submitted && !downloadUrl && <div className='w-fit mx-auto mt-10 flex text-sm gap-2'>
+                <AiOutlineLoading3Quarters className='animate-spin text-gray-500 mt-0.5'/>
+                <div className='text-gray-500'>fetching reel</div>
+            </div>}
 
 
-        {downloadUrl ? <div>
-            <div>Your video is ready for download</div>
-            <video controls width='100%' src={downloadUrl} className='w-1/2'/>
-            <a href={`https://proxy-server-rd.vercel.app/download?downloadUrl=${encodeURIComponent(downloadUrl)}`} download='instagram-reel.mp4' className='px-5 py-2 rounded-lg bg-black text-white'>Download Reel</a>
-        </div> : <div>loading...</div>}
-
-    </div>
-  );
+        </div>
+    );
 }
